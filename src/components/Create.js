b638a56c182520cc7f9de8cbced22a265d5add28
g00360704 <113925227+g00360704@users.noreply.github.com>
2023-10-17 17:46:38 +0100

    Update Create.js

--- src/components/Create.js
+++ src/components/Create.js
@@ -1,9 +1,58 @@
-function Create(){
-    return(
-        <div>
-            <h2>Hello from my Create component</h2>
-        </div>
-    );
+import { useState } from "react";
+
+function Create() {
+
+    const [title, setTitle] = useState('');
+    const [cover, setCover] = useState('');
+    const [author, setAuthor] = useState('');
+    
+    const handleSubmit = (e) => {
+    e.preventDefault();
+
+    console.log("Title: "+title+
+    " Cover:" +cover+
+    " Author: " +cover);
     }
     
-    export default Create;
+    return (
+        <div>
+            <h2>This is my Create Component</h2>
+            <form onSubmit={handleSubmit}>
+                <div className="form-group">
+                    <label>Add Book Title: </label>
+                    <input type="text"
+                        className="form-control"
+                        value={title}
+                        onChange={(e) => { setTitle(e.target.value) }}
+                    />
+                </div>
+
+                <div className="form-group">
+                    <label>Add Book Cover: </label>
+                    <input type="text"
+                        className="form-control"
+                        value={cover}
+                        onChange={(e) => { setCover(e.target.value) }}
+                    />
+                </div>
+
+                <div className="form-group">
+                    <label>Add Book Author: </label>
+                    <input type="text"
+                        className="form-control"
+                        value={author}
+                        onChange={(e) => { setAuthor(e.target.value) }}
+                    />
+                </div>
+                <div>
+          <input
+            type="submit"
+            value="Create Book"
+          />
+        </div>
+            </form>
+        </div>
+    );
+}
+
+export default Create;
