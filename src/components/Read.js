b8ddff66fcb6c4dcfeca072af2e7658c0b16f334
g00360704 <113925227+g00360704@users.noreply.github.com>
2023-10-17 17:51:47 +0100

    Update Read.js

--- src/components/Read.js
+++ src/components/Read.js
@@ -1,54 +1,33 @@
-import Books from "./Books";
+import { useEffect, useState } from "react";
+import Books from "./books";
+import axios from "axios";
 
-function Read(props){
+function Read() {
 
-    const data = 
 
-    [
-        {
-            "title": "Learn Git in a Month of Lunches",
-            "isbn": "1617292419",
-            "pageCount": 0,
-            "thumbnailUrl":
-            "https://s3.amazonaws.com/AKIAJC5RLADLUMVRPFDQ.book-thumb-images/umali.jpg",
-            "status": "MEAP",
-            "authors": ["Rick Umali"],
-            "categories": []
-            },
-            {
-            "title": "MongoDB in Action, Second Edition",
-            "isbn": "1617291609",
-            "pageCount": 0,
-            "thumbnailUrl":
-            "https://s3.amazonaws.com/AKIAJC5RLADLUMVRPFDQ.book-thumb-images/banker2.jpg",
-            "status": "MEAP",
-            "authors": [
-            "Kyle Banker",
-            "Peter Bakkum",
-            "Tim Hawkins",
-            "Shaun Verch",
-            "Douglas Garrett"
-            ],
-            "categories": []
-            },
-            {
-            "title": "Getting MEAN with Mongo, Express, Angular, and Node",
-            "isbn": "1617292036",
-            "pageCount": 0,
-            "thumbnailUrl":
-            "https://s3.amazonaws.com/AKIAJC5RLADLUMVRPFDQ.book-thumb-images/sholmes.jpg",
-            "status": "MEAP",
-            "authors": ["Simon Holmes"],
-            "categories": []
-            }
-                
-        ];
-return(
-    <div>
-        <h2>Hello from my Read component</h2>
-        <Books myBooks={data}></Books>
-    </div>
-);
+    const [data, setData] = useState([]);
+//useEffect is a React Hook that lets you synchronize a component with an external system.
+    useEffect(() => {
+        axios.get('https://jsonblob.com/api/jsonblob/1161593332966481920')
+            .then(
+                (response) => {
+                    setData(response.data.books);
+                }
+            )
+            .catch(
+                (error) => {
+                    console.log(error);
+                }
+            );
+    }, []);
+
+    return (
+        <div>
+            <h2>Hello from my Read Component</h2>
+            <Books myBooks={data}></Books>
+        </div>
+    );
+
 }
 
 export default Read;
