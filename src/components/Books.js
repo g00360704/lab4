a269c44f7efc87d05d2d70ddd9923d27969eff7b
g00360704 <113925227+g00360704@users.noreply.github.com>
2023-10-17 17:43:16 +0100

    Update Books.js

--- src/components/Books.js
+++ src/components/Books.js
@@ -1,11 +1,14 @@
 import BookItem from "./BookItem";
 
 function Books(props){
+    // This component takes a 'props' object as an argument.
+  // It is intended to render a list of books passed in 'props.myBooks'.
     return props.myBooks.map(
+         // Use the map() function to iterate over each book in 'props.myBooks' and generate a list of BookItem components.
         (book)=>{ 
             return <BookItem mybook={book} key={book.isbn}></BookItem>
         }
 );
 }
-
+// Export the Books component for use in other parts of the application.
 export default Books;
