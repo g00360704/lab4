787612e203ae6a6fc7bdcb8d7e2fc9d8086098e9
g00360704 <113925227+g00360704@users.noreply.github.com>
2023-10-17 17:54:28 +0100

    Update Create.js

--- src/components/Create.js
+++ src/components/Create.js
@@ -1,19 +1,24 @@
 import { useState } from "react";
 
 function Create() {
+    // Define a React component called 'Create' for adding new books with a form.
 
+    // Initialize state variables for book title, cover, and author using the 'useState' hook.
     const [title, setTitle] = useState('');
     const [cover, setCover] = useState('');
     const [author, setAuthor] = useState('');
-    
-    const handleSubmit = (e) => {
-    e.preventDefault();
 
-    console.log("Title: "+title+
-    " Cover:" +cover+
-    " Author: " +cover);
+    // Define a function to handle form submission.
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        // Log the entered book information (title, cover, author) when the form is submitted.
+        console.log("Title: " + title +
+            " Cover: " + cover +
+            " Author: " + author);
     }
-    
+
+    // Render the create book form with input fields and a submit button.
     return (
         <div>
             <h2>This is my Create Component</h2>
@@ -45,11 +50,11 @@ function Create() {
                     />
                 </div>
                 <div>
-          <input
-            type="submit"
-            value="Create Book"
-          />
-        </div>
+                    <input
+                        type="submit"
+                        value="Create Book"
+                    />
+                </div>
             </form>
         </div>
     );
