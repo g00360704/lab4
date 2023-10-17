09fa0cc570b205d7e48c9f7d9b6489f5b19b0939
g00360704 <113925227+g00360704@users.noreply.github.com>
2023-10-17 17:53:23 +0100

    Update Read.js

--- src/components/Read.js
+++ src/components/Read.js
@@ -3,14 +3,17 @@ import Books from "./books";
 import axios from "axios";
 
 function Read() {
+    // Define a React component called 'Read' responsible for fetching and displaying book data.
 
-
+    // Initialize a 'data' state variable using the 'useState' hook.
     const [data, setData] = useState([]);
-//useEffect is a React Hook that lets you synchronize a component with an external system.
+
+    // Use the 'useEffect' hook to fetch book data from an external API when the component mounts.
     useEffect(() => {
         axios.get('https://jsonblob.com/api/jsonblob/1161593332966481920')
             .then(
                 (response) => {
+                    // Update the 'data' state with the fetched book data.
                     setData(response.data.books);
                 }
             )
@@ -21,13 +24,14 @@ function Read() {
             );
     }, []);
 
+    // Render a message and the 'Books' component, passing the fetched book data as a prop.
     return (
         <div>
             <h2>Hello from my Read Component</h2>
             <Books myBooks={data}></Books>
         </div>
     );
-
 }
 
 export default Read;
+
